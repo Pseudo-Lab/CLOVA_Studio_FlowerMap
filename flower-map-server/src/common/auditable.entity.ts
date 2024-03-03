727b73dbc71957ddfc2ae2b08ce203ccfd7c028b
Ksiyeong <philip2767@gmail.com>
2024-03-03 12:40:21 +0900

    refactor: 컬럼명 snake_case로 변경

--- flower-map-server/src/common/auditable.entity.ts
+++ flower-map-server/src/common/auditable.entity.ts
@@ -1,12 +1,21 @@
 import { BaseEntity, CreateDateColumn, DeleteDateColumn, UpdateDateColumn } from "typeorm";
 
 export abstract class Auditable extends BaseEntity {
-    @CreateDateColumn()
+    @CreateDateColumn({    // type: 'datetime', width: 4,
+        name: 'created_at',
+        nullable: false
+    })
     createdAt: Date;
 
-    @UpdateDateColumn()
+    @UpdateDateColumn({
+        name: 'modified_at',
+        nullable: false
+    })
     modifiedAt: Date;
 
-    @DeleteDateColumn()
+    @DeleteDateColumn({
+        name: 'deleted_at',
+        nullable: true
+    })
     deletedAt: Date | null;
 }
