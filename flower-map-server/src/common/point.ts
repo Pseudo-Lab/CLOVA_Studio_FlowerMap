75559a67fadf55abd5712cfc9442bc7bd966ce28
Ksiyeong <philip2767@gmail.com>
2024-03-03 23:15:16 +0900

    feat: point 파싱 생성자 추가

--- flower-map-server/src/common/point.ts
+++ flower-map-server/src/common/point.ts
@@ -1,6 +1,18 @@
+import { InternalServerErrorException } from "@nestjs/common";
 import { IsNotEmpty } from "class-validator";
 
 export class Point {
+    constructor(pointString: string) {
+        const match = pointString.match(/\(([^)]+)\)/);
+        if (match && match.length > 1) {
+            const split = match[1].split(' ');
+            this.latitude = parseFloat(split[0]);
+            this.longitude = parseFloat(split[1]);
+        } else {
+            //TODO 예외처리 구체화할 것.
+            throw new InternalServerErrorException();
+        }
+    }
 
     @IsNotEmpty()
     latitude: number;
