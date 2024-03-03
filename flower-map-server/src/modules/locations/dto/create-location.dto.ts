ff614b4aa0d0937dd15ceaa8180ecd24dbb143a6
Ksiyeong <philip2767@gmail.com>
2024-03-03 17:34:56 +0900

    feat: CreateLocationDto

--- flower-map-server/src/modules/locations/dto/create-location.dto.ts
+++ flower-map-server/src/modules/locations/dto/create-location.dto.ts
@@ -1,1 +1,34 @@
-export class CreateLocationDto {}
+import { Flower } from "src/modules/flowers/entities/flower.entity";
+import { Point } from "../../../common/point";
+import { Location } from "../entities/location.entity";
+import { IsNotEmpty, IsNumber, IsString, ValidateNested } from "class-validator";
+import { Type } from "class-transformer";
+
+export class CreateLocationDto {
+
+    @IsString()
+    numberAddress: string;
+
+    @IsString()
+    roadAddress: string;
+
+    @IsNotEmpty()
+    @ValidateNested()
+    @Type(() => Point)
+    coordinates: Point;
+
+    @IsNumber()
+    flowerId: number;
+
+    public toEntity(): Location {
+        const location = new Location()
+        const flower = new Flower();
+        flower.flowerId = this.flowerId;
+        location.numberAddress = this.numberAddress;
+        location.roadAddress = this.roadAddress;
+        location.coordinates = this.coordinates.toPointString();
+        location.flower = flower;
+        return location;
+    }
+
+}
