00936b9a577a0c7814c8d54a055f235e50a2ab25
Ksiyeong <philip2767@gmail.com>
2024-03-03 13:09:42 +0900

    feat: Flower 엔티티 추가

--- flower-map-server/src/modules/flowers/entities/flower.entity.ts
+++ flower-map-server/src/modules/flowers/entities/flower.entity.ts
@@ -1,1 +1,17 @@
-export class Flower {}
+import { Auditable } from "src/common/auditable.entity";
+import { Location } from "src/modules/locations/entities/location.entity";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Flower extends Auditable {
+
+    @PrimaryGeneratedColumn({ name: 'flower_id' })
+    flowerId: number;
+
+    @Column()
+    name: string;
+
+    @OneToMany(() => Location, (location) => location.flower)
+    locations: Location[];
+
+}
