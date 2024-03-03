c291b7b7fa61d2314f85668f1630610fc8827cc6
Ksiyeong <philip2767@gmail.com>
2024-03-03 23:54:58 +0900

    Feat: Location 생성, 단건조회 구현

--- flower-map-server/src/modules/locations/locations.controller.ts
+++ flower-map-server/src/modules/locations/locations.controller.ts
@@ -1,34 +1,36 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe } from '@nestjs/common';
 import { LocationsService } from './locations.service';
 import { CreateLocationDto } from './dto/create-location.dto';
 import { UpdateLocationDto } from './dto/update-location.dto';
+import { ResponseLocationDto } from './dto/response-location.dto';
 
 @Controller('api/v1/locations')
 export class LocationsController {
-  constructor(private readonly locationsService: LocationsService) {}
+  constructor(private readonly locationsService: LocationsService) { }
 
   @Post()
-  create(@Body() createLocationDto: CreateLocationDto) {
-    return this.locationsService.create(createLocationDto);
+  async create(@Body() createLocationDto: CreateLocationDto): Promise<ResponseLocationDto> {
+    const location = await this.locationsService.create(createLocationDto);
+    return new ResponseLocationDto(location);
   }
 
-  @Get()
-  findAll() {
-    return this.locationsService.findAll();
+  // @Get()
+  // findAll() {
+  //   return this.locationsService.findAll();
+  // }
+
+  @Get(':locationId')
+  async findOne(@Param('locationId', ParseIntPipe) locationId: number): Promise<ResponseLocationDto> {
+    return new ResponseLocationDto(await this.locationsService.findOne(locationId));
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.locationsService.findOne(+id);
-  }
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateLocationDto: UpdateLocationDto) {
+  //   return this.locationsService.update(+id, updateLocationDto);
+  // }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateLocationDto: UpdateLocationDto) {
-    return this.locationsService.update(+id, updateLocationDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.locationsService.remove(+id);
-  }
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.locationsService.remove(+id);
+  // }
 }
--- flower-map-server/src/modules/locations/locations.service.ts
+++ flower-map-server/src/modules/locations/locations.service.ts
@@ -1,19 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateLocationDto } from './dto/create-location.dto';
 import { UpdateLocationDto } from './dto/update-location.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Location } from './entities/location.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class LocationsService {
-  create(createLocationDto: CreateLocationDto) {
-    return 'This action adds a new location';
+  constructor(
+    @InjectRepository(Location)
+    private locationsRepository: Repository<Location>,
+  ) { }
+
+  async create(createLocationDto: CreateLocationDto): Promise<Location> {
+    return await this.locationsRepository
+      .create(createLocationDto.toEntity())
+      .save();
   }
 
   findAll() {
     return `This action returns all locations`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} location`;
+  async findOne(locationId: number): Promise<Location> {
+    const location = await this.locationsRepository.findOneBy({ locationId });
+    if (location) return location;
+    else throw new NotFoundException(); //TODO 예외 구체화할것
   }
 
   update(id: number, updateLocationDto: UpdateLocationDto) {
