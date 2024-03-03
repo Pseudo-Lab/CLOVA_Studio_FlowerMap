a8961a2e8c20dd0f713a96fa9632ee27b3de10cb
Ksiyeong <philip2767@gmail.com>
2024-03-03 17:31:45 +0900

    feat: 데이터 유효성 검사 실시

--- flower-map-server/src/main.ts
+++ flower-map-server/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // 데이터 유효성 검사
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true, // 선언하지 않은 데이터 입력시 400 예외
+    transform: true, // 특정 객체 인스턴스로 정상 변환
+  }));
+
   await app.listen(3000);
 }
 bootstrap();
