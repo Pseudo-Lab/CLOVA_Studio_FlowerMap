f6a5f587ffdca16f705388266c2c0685da106fe0
Ksiyeong <philip2767@gmail.com>
2024-03-05 23:06:28 +0900

    docs: API 문서 설명 추가 및 기타 기본 API 주석처리

--- flower-map-server/src/config/swagger.config.ts
+++ flower-map-server/src/config/swagger.config.ts
@@ -4,8 +4,15 @@ import { INestApplication } from '@nestjs/common';
 export function setupSwagger(app: INestApplication) {
     const config = new DocumentBuilder()
         .setTitle('Flower Map API Document')
-        .setDescription('The Flower Map API description')
-        .setVersion('1.0')
+        .setDescription(`
+        The Flower Map API description
+
+        현재 프로토타입 버전으로 모든 API에 대한 호출 결과는 더미데이터로 이루어져있습니다.
+        API 추가 요청, 변경 등 기타 문의 사항은 Discord back 채널에 남겨주세요.
+
+        writer: 김시영
+        `)
+        .setVersion('프로토타입')
         .addTag('flower maps')
         .build();
     const document = SwaggerModule.createDocument(app, config);
