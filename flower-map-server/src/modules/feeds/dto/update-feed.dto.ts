62b1cc1f04540420f99ec1aadbb6402ff1f616ba
Ksiyeong <philip2767@gmail.com>
2024-03-05 15:27:35 +0900

    feat: Feed 수정 dto 추가

--- flower-map-server/src/modules/feeds/dto/update-feed.dto.ts
+++ flower-map-server/src/modules/feeds/dto/update-feed.dto.ts
@@ -1,4 +1,29 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateFeedDto } from './create-feed.dto';
+import { ApiProperty } from '@nestjs/swagger';
 
-export class UpdateFeedDto extends PartialType(CreateFeedDto) {}
+// export class UpdateFeedDto extends PartialType(CreateFeedDto) {}
+export class UpdateFeedDto {
+
+    @ApiProperty()
+    content: string;
+
+    @ApiProperty()
+    nickname: string;
+
+    @ApiProperty()
+    password: string;
+
+    @ApiProperty()
+    capturedAt: string;
+
+    @ApiProperty()
+    floweringStatus: number;
+
+    @ApiProperty()
+    photos: string;
+
+    @ApiProperty()
+    location: string;
+
+}
