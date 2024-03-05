520bf922ced846455b3d184a80aee0de18accb2e
Ksiyeong <philip2767@gmail.com>
2024-03-05 15:27:55 +0900

    feat: Feed 생성 dto 추가

--- flower-map-server/src/modules/feeds/dto/create-feed.dto.ts
+++ flower-map-server/src/modules/feeds/dto/create-feed.dto.ts
@@ -1,15 +1,25 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class CreateFeedDto {
 
+    @ApiProperty({ description: '피드에 대한 간단한 설명입니다.' })
     content: string;
 
+    @ApiProperty()
     nickname: string;
 
+    @ApiProperty()
     password: string;
 
+    @ApiProperty()
     capturedAt: Date;
 
+    @ApiProperty({ description: '개화상태 레벨입니다. 1 ~ 5단계 별로 나타냅니다.' })
     floweringStatus: number;
 
+    @ApiProperty({ description: 'Location 식별자 입니다.' })
     locationId: number;
 
+    @ApiProperty({ description: '사진과 관련된 내용, 관련 내용 개발중입니다.' })
+    photo: string;
 }
