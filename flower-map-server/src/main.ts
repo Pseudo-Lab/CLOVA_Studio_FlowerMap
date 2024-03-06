473aaf20f8c0068411d67e5c3db300aca09690c4
Ksiyeong <philip2767@gmail.com>
2024-03-06 11:36:21 +0900

    refactor: PORT번호 환경변수로 은닉

--- flower-map-server/src/main.ts
+++ flower-map-server/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { setupSwagger } from './config/swagger.config';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
 
   // swagger API 문서화 설정
   setupSwagger(app);
@@ -15,6 +17,7 @@ async function bootstrap() {
     transform: true, // 특정 객체 인스턴스로 정상 변환
   }));
 
-  await app.listen(3000);
+  const SERVER_PORT = configService.get<number>('SERVER_PORT');
+  await app.listen(SERVER_PORT, () => Logger.log(`Application running on port ${SERVER_PORT}`));
 }
 bootstrap();
