2a6f82c7e481f5b9f542e0322e450768fb51f214
Ksiyeong <philip2767@gmail.com>
2024-03-08 00:29:46 +0900

    refactor: 필드명 수정 index->idx

--- flower-map-server/src/modules/images/entities/image.entity.ts
+++ flower-map-server/src/modules/images/entities/image.entity.ts
@@ -1,6 +1,6 @@
 import { Auditable } from "src/common/auditable.entity";
 import { Feed } from "src/modules/feeds/entities/feed.entity";
-import { Location } from "src/modules/locations/entities/location.entity";
+import { Flower } from "src/modules/flowers/entities/flower.entity";
 import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
@@ -9,8 +9,8 @@ export class Image extends Auditable {
     @PrimaryGeneratedColumn({ name: 'image_id' })
     imageId: number;
 
-    @Column({ name: 'uploader_ip' })
-    uploaderIp: string;
+    @Column({ name: 'user_ip' })
+    userIp: string;
 
     @Column({ name: 'origin_url' })
     originUrl: string;
@@ -19,14 +19,14 @@ export class Image extends Auditable {
     thumbUrl: string;
 
     @Column({ nullable: true })
-    index: number;
-
-    @ManyToOne(() => Location, (location) => location.images, { nullable: true })
-    @JoinColumn({ name: 'location_id' })
-    location: Location;
+    idx: number;
 
     @ManyToOne(() => Feed, (feed) => feed.images, { nullable: true })
     @JoinColumn({ name: 'feed_id' })
     feed: Feed;
+
+    @ManyToOne(() => Flower, (flower) => flower.images)
+    @JoinColumn({ name: 'flower_id' })
+    flower: Flower;
 
 }
