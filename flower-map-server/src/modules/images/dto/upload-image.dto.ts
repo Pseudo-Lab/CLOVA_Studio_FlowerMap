a8726bc91749d9585d7ddd3394d3fdae2da54cb9
Ksiyeong <philip2767@gmail.com>
2024-03-11 16:25:34 +0900

    feat: UploadImageDto 추가

---
+++ flower-map-server/src/modules/images/dto/upload-image.dto.ts
@@ -0,0 +1,13 @@
+import { ApiProperty } from "@nestjs/swagger";
+
+export class UploadImageDto {
+
+    @ApiProperty({
+        description: '업로드할 이미지를 등록해주세요.',
+        required: true,
+        type: 'string',
+        format: 'binary'
+    })
+    image: Express.Multer.File; // Express.Multer.File와 같은 타입 사용
+
+}
