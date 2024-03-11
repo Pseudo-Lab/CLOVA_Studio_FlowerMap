c7bc7f03210096d69e514afb1f6f6b85aa6ee487
Ksiyeong <philip2767@gmail.com>
2024-03-11 16:26:37 +0900

    feat: Image 저장 기능 구현

--- flower-map-server/src/modules/images/images.service.ts
+++ flower-map-server/src/modules/images/images.service.ts
@@ -1,26 +1,17 @@
 import { Injectable } from '@nestjs/common';
-import { CreateImageDto } from './dto/create-image.dto';
-import { UpdateImageDto } from './dto/update-image.dto';
+import { Image } from './entities/image.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ImagesService {
-  create(createImageDto: CreateImageDto) {
-    return 'This action adds a new image';
+  constructor(
+    @InjectRepository(Image)
+    private feedsRepository: Repository<Image>,
+  ) { }
+
+  async create(image: Image): Promise<Image> {
+    return await this.feedsRepository.create(image).save();
   }
 
-  findAll() {
-    return `This action returns all images`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} image`;
-  }
-
-  update(id: number, updateImageDto: UpdateImageDto) {
-    return `This action updates a #${id} image`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} image`;
-  }
 }
