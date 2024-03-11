06f50c5a9c60c3785b50f685db0ff4d0f9d0466f
Ksiyeong <philip2767@gmail.com>
2024-03-11 16:26:16 +0900

    feat: 이미지 레포지토리 사용 등록

--- flower-map-server/src/modules/images/images.module.ts
+++ flower-map-server/src/modules/images/images.module.ts
@@ -3,8 +3,11 @@ import { ImagesService } from './images.service';
 import { ImagesController } from './images.controller';
 import { ImagesEditingService } from './images-editing.service';
 import { ImagesUploadService } from './images-upload.service';
+import { Image } from './entities/image.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Image])],
   controllers: [ImagesController],
   providers: [ImagesService, ImagesEditingService, ImagesUploadService],
 })
