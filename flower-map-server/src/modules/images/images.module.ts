d7dfe8ca179bb733299b6d5ef6b0f6a4d9cb9f10
Ksiyeong <philip2767@gmail.com>
2024-03-11 15:34:52 +0900

    feat: 이미지 편집 업로드 서비스 등록

--- flower-map-server/src/modules/images/images.module.ts
+++ flower-map-server/src/modules/images/images.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ImagesService } from './images.service';
 import { ImagesController } from './images.controller';
+import { ImagesEditingService } from './images-editing.service';
+import { ImagesUploadService } from './images-upload.service';
 
 @Module({
   controllers: [ImagesController],
-  providers: [ImagesService],
+  providers: [ImagesService, ImagesEditingService, ImagesUploadService],
 })
-export class ImagesModule {}
+export class ImagesModule { }
