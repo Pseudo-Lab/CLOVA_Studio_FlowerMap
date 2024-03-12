4b4a2e71150a24043167d74ca9afc5cd62c263f2
Ksiyeong <philip2767@gmail.com>
2024-03-12 16:12:14 +0900

    feat: ETage 추가

--- flower-map-server/src/modules/images/dto/response-image.dto.ts
+++ flower-map-server/src/modules/images/dto/response-image.dto.ts
@@ -13,13 +13,21 @@ export class ResponseImageDto {
     originUrl: string;
 
     @ApiProperty()
+    originETag: string;
+
+    @ApiProperty()
     thumbUrl: string;
+
+    @ApiProperty()
+    thumbETag: string;
 
     constructor(image: Image) {
         this.imageId = image.imageId;
         this.idx = image.idx;
         this.originUrl = image.originUrl;
+        this.originETag = image.originETag;
         this.thumbUrl = image.thumbUrl;
+        this.thumbETag = image.thumbETag;
     }
 
 }
