c7791f9e1d6a0765e10ca3d3ccf5ebdede9a8ce5
Ksiyeong <philip2767@gmail.com>
2024-03-12 16:14:44 +0900

    feat: Feed 단건조회 기능구현 GET /api/v1/feeds/:feedId

--- flower-map-server/src/modules/feeds/feeds.controller.ts
+++ flower-map-server/src/modules/feeds/feeds.controller.ts
@@ -77,29 +77,14 @@ export class FeedsController {
     // return this.feedsService.findAll();
   }
 
-  @Get(':feedId')
-  @ApiOperation({ summary: 'Feed 단건 조회 (미완)', description: '특정 Feed의 상세정보를 조회합니다.' })
+  // API 문서
+  @ApiOperation({ summary: 'Feed 단건 조회', description: '특정 Feed의 상세정보를 조회합니다.' })
   @ApiOkResponse({ description: '요청 성공', type: ResponseFeedDto })
-  @ApiNotFoundResponse({ description: '요청하신 Feed가 존재하지 않습니다.' })
-  findOne(@Param('feedId') feedId: number) {
+  @ApiNotFoundResponse({ description: `요청하신 Feed가 존재하지 않습니다. [errorCode=${CustomErrorCode.FEED_NOT_FOUND}]` })
 
-    const feed = new Feed();
-    feed.feedId = feedId;
-    feed.content = '반가워요 피드 더미에요!';
-    feed.capturedAt = new Date();
-    feed.floweringStatus = Math.floor(Math.random() * 5) + 1;
-    const image = new Image()
-    image.imageId = 1;
-    image.idx = 1;
-    image.originUrl = 'https://item.kakaocdn.net/do/71b0683bd1963c4e24c8ba605e23bac9617ea012db208c18f6e83b1a90a7baa7';
-    image.thumbUrl = 'https://item.kakaocdn.net/do/ffd6fd4ddd308f7b129cf04c5ca71ada617ea012db208c18f6e83b1a90a7baa7';
-    feed.addImage(image);
-    feed.addImage(image);
-    feed.addImage(image);
-    feed.addImage(image);
-
-    return new ResponseFeedDto(feed);
-    // return this.feedsService.findOne(feedId);
+  @Get(':feedId')
+  async findOne(@Param('feedId') feedId: number): Promise<ResponseFeedDto> {
+    return new ResponseFeedDto(await this.feedsService.findOne(feedId));
   }
 
   @Patch(':feedId')
--- flower-map-server/src/modules/feeds/feeds.service.ts
+++ flower-map-server/src/modules/feeds/feeds.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { UpdateFeedDto } from './dto/update-feed.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Feed } from './entities/feed.entity';
 import { Repository } from 'typeorm';
 import * as bcrypt from 'bcrypt';
+import { CustomErrorCode } from 'src/common/exception/custom-error-code';
 
 @Injectable()
 export class FeedsService {
@@ -26,8 +27,15 @@ export class FeedsService {
     return `This action returns all feeds`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} feed`;
+  /**
+   * 특정 Feed에 대한 정보 가져오기
+   * 연관관계 : Image, Location
+   */
+  // TODO Heart 관련
+  async findOne(feedId: number): Promise<Feed> {
+    const feed = await this.feedsRepository.findOne({ where: { feedId }, relations: ['images', 'location'] });
+    if (!feed) throw new NotFoundException(CustomErrorCode.FEED_NOT_FOUND);
+    else return feed;
   }
 
   update(id: number, updateFeedDto: UpdateFeedDto) {
