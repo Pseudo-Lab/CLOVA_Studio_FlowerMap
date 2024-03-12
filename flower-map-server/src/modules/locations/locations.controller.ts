0df69a3af69f997af46ab3d9dfa7e2b308ad2b91
Ksiyeong <philip2767@gmail.com>
2024-03-12 21:20:22 +0900

    feat: Location 단건 조회 기능 구현 GET /api/v1/locations/:locationId

--- flower-map-server/src/modules/locations/locations.controller.ts
+++ flower-map-server/src/modules/locations/locations.controller.ts
@@ -1,14 +1,13 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe, Query } from '@nestjs/common';
 import { LocationsService } from './locations.service';
 import { CreateLocationDto } from './dto/create-location.dto';
-import { UpdateLocationDto } from './dto/update-location.dto';
 import { ResponseLocationDto } from './dto/response-location.dto';
 import { ApiBadRequestResponse, ApiCreatedResponse, ApiNotFoundResponse, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { SingleResponseDto } from 'src/common/single-response.dto';
 import { ResponsePageDto } from 'src/common/response-page.dto';
 import { Location } from './entities/location.entity';
-import { Point } from 'src/common/point';
 import { Flower } from '../flowers/entities/flower.entity';
+import { CustomErrorCode } from 'src/common/exception/custom-error-code';
 
 @ApiTags('Location(위치정보) API')
 @ApiBadRequestResponse({ description: '잘못된 요청 형식입니다. (body, query, param 등)' })
@@ -44,7 +43,7 @@ export class LocationsController {
       location.locationId = i;
       location.numberAddress = '지번주소' + i
       location.roadAddress = '도로명주소' + i
-      location.coordinates = `POINT(${latitude + i / 1000} ${longitude + i / 1000})`
+      location.coordinates = [latitude + i / 1000, longitude + i / 1000]
       location.flower = flower;
       data[i - 1] = new ResponseLocationDto(location)
     }
@@ -53,21 +52,14 @@ export class LocationsController {
     return new ResponsePageDto<ResponseLocationDto>(100, offset, limit, data);
   }
 
-  @Get(':locationId')
-  @ApiOperation({ summary: 'Location 단건 조회 (미완)', description: '특정 Location을 id로 조회한다.' })
+  // API 문서
+  @ApiOperation({ summary: 'Location 단건 조회', description: '특정 Location을 id로 조회한다.' })
   @ApiOkResponse({ description: '요청 성공', type: ResponseLocationDto })
-  @ApiNotFoundResponse({ description: '요청한 Locatioon이 존재하지 않음' })
+  @ApiNotFoundResponse({ description: `Locatioon이 존재하지 않습니다. [errorCode=${CustomErrorCode.LOCATION_NOT_FOUND}]` })
+
+  @Get(':locationId')
   async findOne(@Param('locationId', ParseIntPipe) locationId: number): Promise<ResponseLocationDto> {
-    // return new ResponseLocationDto(await this.locationsService.findOne(locationId));
-    const location = new Location();
-    location.locationId = locationId;
-    location.numberAddress = '지번주소';
-    location.roadAddress = '도로명주소';
-    location.coordinates = 'POINT(12.123 23.23443)';
-    const flower = new Flower()
-    flower.flowerId = 23;
-    flower.name = '벚꽃';
-    location.flower = flower;
+    const location = await this.locationsService.findOne(locationId);
     return new ResponseLocationDto(location);
   }
 
