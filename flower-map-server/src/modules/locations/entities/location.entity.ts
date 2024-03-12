430e94d02812022a64627fa135e6303d1042411a
Ksiyeong <philip2767@gmail.com>
2024-03-12 19:43:15 +0900

    refactor: 위도 경도 배열 형식 매핑

--- flower-map-server/src/modules/locations/entities/location.entity.ts
+++ flower-map-server/src/modules/locations/entities/location.entity.ts
@@ -15,8 +15,14 @@ export class Location extends Auditable {
     @Column({ name: 'road_address' })
     roadAddress: string;
 
-    @Column({ type: 'point' })
-    coordinates: string;
+    // @Index({ spatial: true })
+    @Column({
+        type: 'point', transformer: {
+            from: (value: string) => value.substring(6, value.length - 1).split(' ').map(parseFloat),
+            to: (value: [number, number]) => `POINT(${value[0]} ${value[1]})`
+        }
+    })
+    coordinates: [number, number]; // 위도, 경도
 
     @ManyToOne(() => Flower, (flower) => flower.locations, { nullable: false })
     @JoinColumn({ name: 'flower_id' })
