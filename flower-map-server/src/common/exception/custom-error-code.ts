a48096d8f20088d1f948ca4d556291458bc66f4f
Ksiyeong <philip2767@gmail.com>
2024-03-14 00:24:44 +0900

    feat: FL404 추가

--- flower-map-server/src/common/exception/custom-error-code.ts
+++ flower-map-server/src/common/exception/custom-error-code.ts
@@ -1,16 +1,22 @@
 export enum CustomErrorCode {
     // 공통
-    INTERNAL_SERVER_ERROR = '500', // 서버 측 에러 관리자 문의 요망
-    VALIDATION_BAD_REQUEST = 'V400', // 데이터 유효성 검사 통과 실패
+    INTERNAL_SERVER_ERROR = '500',// 서버 측 에러 관리자 문의 요망
+    VALIDATION_BAD_REQUEST = 'V400',// 데이터 유효성 검사 통과 실패
+
+    // Flower
+    FLOWER_NOT_FOUND = 'FL404', // 존재하지 않는 Flower
 
     // Feed
-    FEED_NOT_FOUND = 'F404', // 존재하지 않는 Feed
+    FEED_NOT_FOUND = 'F404',// 존재하지 않는 Feed
+    FEED_UNAUTHORIZED = 'FU401', // Feed에 대한 접근 권한이 없음
+
 
     // Location
-    LOCATION_NOT_FOUND = 'L404', // 존재하지 않는 Location
+    LOCATION_NOT_FOUND = 'L404',// 존재하지 않는 Location
+
 
     // Image
-    IMAGE_NOT_FOUND = 'I404', // 존재하지 않는 Image
-    IMAGE_UNSUPPORTED_EXT = 'IUE400', // 지원하지 않는 Image 확장자 타입
-    FEED_UNAUTHORIZED = 'FU401' // Feed에 대한 접근 권한이 없음
+    IMAGE_NOT_FOUND = 'I404',// 존재하지 않는 Image
+    IMAGE_UNSUPPORTED_EXT = 'IUE400',// 지원하지 않는 Image 확장자 타입
+
 }
