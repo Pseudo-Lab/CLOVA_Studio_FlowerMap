90a62361a1ec47e3075af4a28e938739af3d85b3
Ksiyeong <philip2767@gmail.com>
2024-03-19 12:34:52 +0900

    refactor: 공통 부분 분리

--- flower-map-server/src/common/request-page.dto.ts
+++ flower-map-server/src/common/request-page.dto.ts
@@ -1,5 +1,25 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { Type } from "class-transformer";
+import { Max, Min } from "class-validator";
+
 export class RequestPageDto {
-    sort: 'ASC' | 'DESC';
+    @ApiProperty({
+        description: '한번에 가져올 데이터 갯수. 5 ~ 30',
+        example: 10
+    })
+    @Type(() => Number)
+    @Min(5)
+    @Max(30)
     limit: number;
+
+    @ApiProperty({
+        description: `
+        스킵할 데이터 갯수. 최소: 0
+        ex) 0 -> 처음부터 검색, 5 -> 6번째부터 검색
+        `,
+        example: 0
+    })
+    @Type(() => Number)
+    @Min(0)
     offset: number;
 }
