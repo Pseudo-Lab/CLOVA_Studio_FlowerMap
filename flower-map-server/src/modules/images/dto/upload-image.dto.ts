2314f7921291fad650702782a335c05fcc8633c7
Ksiyeong <philip2767@gmail.com>
2024-03-22 12:10:13 +0900

    refactor: 이미지 주소만 반환하도록 변경 POST /api/v1/images

--- flower-map-server/src/modules/images/dto/upload-image.dto.ts
+++ flower-map-server/src/modules/images/dto/upload-image.dto.ts
@@ -10,16 +10,4 @@ export class UploadImageDto {
     })
     image: Express.Multer.File; // Express.Multer.File와 같은 타입 사용
 
-    @ApiProperty({
-        description: 'Flower 식별자',
-        required: true,
-        example: 1
-    })
-    flowerId: number;
-
-    @ApiProperty({
-        description: '개화 레벨 0~4'
-    })
-    floweringStatus: number;
-
 }
