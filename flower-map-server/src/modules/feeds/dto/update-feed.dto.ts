e6e868c6740c810cf5890f079d3ce3c640b13014
Ksiyeong <philip2767@gmail.com>
2024-03-29 23:56:42 +0900

    refactor: content만 받도록 변경

--- flower-map-server/src/modules/feeds/dto/update-feed.dto.ts
+++ flower-map-server/src/modules/feeds/dto/update-feed.dto.ts
@@ -1,24 +1,20 @@
-import { ApiProperty } from '@nestjs/swagger';
+import { ApiProperty, PickType } from '@nestjs/swagger';
+import { CreateFeedDto } from './create-feed.dto';
+import { IsNotEmpty, IsString, Length, Matches } from 'class-validator';
 
-// export class UpdateFeedDto extends PartialType(CreateFeedDto) {}
-export class UpdateFeedDto {
+export class UpdateFeedDto extends PickType(CreateFeedDto, ['content']) {
 
-    @ApiProperty()
-    content: string;
-
-    @ApiProperty()
-    nickname: string;
-
-    @ApiProperty()
-    password: string;
-
-    @ApiProperty({ type: Date })
-    capturedAt: string;
-
-    @ApiProperty({ type: [Number] })
-    imageIds: number[];
-
-    @ApiProperty()
-    locationId: number;
+    @ApiProperty({
+        description: `
+        현재 비밀번호를 입력해주세요.
+        [정규표현식: "^[0-9a-zA-Z]{6}$]" (6자 숫자,영문(소,대문자)]`,
+        example: 'curr12',
+        required: true,
+    })
+    @IsNotEmpty()
+    @IsString()
+    @Length(6, 6)
+    @Matches(/^[0-9a-zA-Z]{6}$/)
+    currentPassword: string;
 
 }
