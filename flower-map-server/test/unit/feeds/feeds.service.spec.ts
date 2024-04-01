e0cc4709d29a601eb4197271b2046e1047f4138e
Ksiyeong <philip2767@gmail.com>
2024-04-01 23:36:21 +0900

    test: findOne 테스트 코드 작성

--- flower-map-server/test/unit/feeds/feeds.service.spec.ts
+++ flower-map-server/test/unit/feeds/feeds.service.spec.ts
@@ -3,6 +3,8 @@ import { FeedsService } from '../../../src/modules/feeds/feeds.service';
 import { Repository } from 'typeorm';
 import { Feed } from 'src/modules/feeds/entities/feed.entity';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { NotFoundException } from '@nestjs/common';
+import { CustomErrorCode } from 'src/common/exception/custom-error-code';
 
 describe('FeedsService', () => {
   let service: FeedsService;
@@ -37,6 +39,36 @@ describe('FeedsService', () => {
 
       // then
       expect(result.password).not.toBe(password);
+    });
+  });
+
+  describe('findOne', () => {
+    it('feedId를 이용하여 Feed를 조회하고 존재할 경우 해당 Feed 반환.', async () => {
+      // given
+      const feedId = 1;
+      const feed = new Feed();
+      feed.feedId = feedId;
+      jest.spyOn(feedsRepository, 'findOne').mockResolvedValue(feed);
+
+      // when
+      const result = await service.findOne(feedId);
+
+      // then
+      expect(result.feedId).toEqual(feedId);
+    });
+
+    it('feedId를 이용하여 Feed를 조회하고 존재하지 않아 F404예외 발생.', async () => {
+      // given
+      const feedId = 1;
+      const feed = new Feed();
+      feed.feedId = feedId;
+      jest.spyOn(feedsRepository, 'findOne').mockResolvedValue(null);
+
+      // when
+      // then
+      await expect((service.findOne(feedId)))
+        .rejects
+        .toThrow(new NotFoundException(CustomErrorCode.FEED_NOT_FOUND));
     });
   });
 
